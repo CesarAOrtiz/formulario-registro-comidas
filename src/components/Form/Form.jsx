6e06f00c1377eec6ffe4f6f6a7d3cf8bbbeb8e8f
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2022-03-25 14:25:59 -0400

    Crear componentes para checkbox y textarea

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,100 +2,65 @@ import React from "react";
 import Input from "./Input";
 
 export default function Form() {
-  const normal =
-    "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer";
-  const error =
-    "text-red-500 border-red-500 focus:ring-red-500 focus:border-red-500";
-
-  const labelNormal =
-    "absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6";
-  const labelError = "text-red-500 peer-focus:text-red-500";
   return (
     <form className="w-[380px] p-2">
       <div className="grid ">
         <div className="relative z-0 mb-6 w-full group">
-          <Input helperText="Error" label="Second Name" />
-        </div>
-        <div className="relative z-0 mb-6 w-full group">
-          <input
-            type="text"
-            name="floating_last_name"
-            id="floating_last_name"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
+          <Input
+            name="floating_first_name"
+            helperText="Error"
+            label="First Name"
             required
           />
-          <label
-            htmlFor="floating_last_name"
-            className="absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Last name
-          </label>
+        </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <Input
+            name="floating_last_name"
+            helperText="Error"
+            label="Last Name"
+            required
+          />
         </div>
       </div>
       <div className="grid ">
         <div className="relative z-0 mb-6 w-full group">
-          <input
+          <Input
             type="number"
             name="floating_age"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
+            helperText="Error"
+            label="Age"
             required
           />
-          <label
-            htmlFor="floating_age"
-            className="absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Age
-          </label>
         </div>
         <div className="relative z-0 mb-6 w-full group">
-          <input
+          <Input
             type="email"
             name="floating_email"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
+            helperText="Error"
+            label="Email addressge"
             required
           />
-          <label
-            htmlFor="floating_email"
-            className="absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Email address
-          </label>
         </div>
       </div>
       <div className="grid ">
         <div className="relative z-0 mb-6 w-full group">
-          <input
+          <Input
             type="text"
             name="floating_address"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
+            helperText="Error"
+            label="Address"
             required
           />
-          <label
-            htmlFor="floating_address"
-            className="absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Address
-          </label>
         </div>
         <div className="relative z-0 mb-6 w-full group">
-          <input
+          <Input
             type="tel"
             pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
             name="floating_phone"
-            id="floating_phone"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
+            helperText="Error"
+            label="Phone number (123-456-7890)"
+            required
           />
-          <label
-            htmlFor="floating_phone"
-            className="absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Phone number (123-456-7890)
-          </label>
         </div>
       </div>
       <div className="relative z-0 mb-6 w-full group">
