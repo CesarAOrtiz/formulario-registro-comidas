6725b8137d62c698bd513c9295e573f04db6504b
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2022-03-26 01:41:40 -0400

    Brand bold text agregado al header

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
     <RecordsProvider>
       <div className="min-h-screen w-full flex flex-col items-center bg-gray-100">
         <div className="container">
-          <Card className="bg-blue-700 text-white shadow-xl rounded-xl w-full my-10">
+          <Card className="bg-blue-700 text-white text-lg font-bold uppercase !shadow-xl rounded-xl w-full my-10">
             Brand
           </Card>
           <div className="flex justify-evenly w-full">
-            <Card className="bg-white shadow-xl rounded-xl min-w-[250px] w-[90%] max-w-[380px] max-h-[660px] mr-4">
+            <Card className="bg-white !shadow-xl rounded-xl min-w-[250px] w-[90%] max-w-[380px] max-h-[660px] mr-4">
               <Form />
             </Card>
 
-            <Card className="px-0 py-0 shadow-xl rounded-xl bg-white overflow-auto max-w-[90%] max-h-[660px]">
+            <Card className="!p-0 !shadow-xl rounded-xl bg-white overflow-auto max-w-[90%] max-h-[660px]">
               <Table />
             </Card>
           </div>
